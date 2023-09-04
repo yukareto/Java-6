6ea8aa8320e52b7ed6ad2295c1daef2e90e9d4f8
yureto <yut465p@gamil.com>
2023-09-04 15:32:37 +0900

    第6回課題SprihgBootで、HelloworldからMapの型を使用してmessage,Good moring yuretoに変更

--- src/main/java/com/hello/hello/HelloController.java
+++ src/main/java/com/hello/hello/HelloController.java
@@ -3,11 +3,13 @@ package com.hello.hello;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello world!";
+    public Map<String,String> hello() {
+        return Map.of("message", "Good moring yureto");
     }
 }
